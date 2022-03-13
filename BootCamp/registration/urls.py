13e45321fc997c2342b57fdc1f158a4f4d940a0a
poxiq <poxiq03@gmail.com>
2022-03-13 17:32:57 +0300

    add login and logout endpoint

--- BootCamp/registration/urls.py
+++ BootCamp/registration/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.RegistrationUserView.as_view(), name='registration'),
     path('check/', views.Check.as_view(), name='check'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
