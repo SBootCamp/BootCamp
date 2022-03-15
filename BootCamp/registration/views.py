54023f582a842eca3190a0ef5a4133a1ffb432c6
poxiq <poxiq03@gmail.com>
2022-03-15 15:36:01 +0300

    edit login, logout

--- BootCamp/registration/views.py
+++ BootCamp/registration/views.py
@@ -1,10 +1,13 @@
+import os
+
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+from django.db import IntegrityError
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate, login, logout
 
 from .models import Profile
 from .serializers import ProfileSerializerView
@@ -12,7 +15,6 @@ from .serializers import ProfileSerializerView
 
 class RegistrationUserView(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
     serializer_class = ProfileSerializerView
 
     def get(self, request):
@@ -21,8 +23,12 @@ class RegistrationUserView(APIView):
     def post(self, request):
         serializer = ProfileSerializerView(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            registration_view(request, request.data)
-        return Response({"success": "please wait, your account is verified"})
+            try:
+                if User.objects.get(username=request.data['user.username']):
+                    return Response({"message": "пользователь с таким 'username' уже существует"})
+            except User.DoesNotExist:
+                registration_view(request, request.data)
+                return Response({"success": "please wait, your account is verified"})
 
 
 def registration_view(request, validated_data):
@@ -36,14 +42,15 @@ def registration_view(request, validated_data):
 
 def send_link(new_link):
     send_mail(
-        'Subject',
+        'Create new profile',
         str(new_link),
-        'poxiq03@mail.ru',
-        ['poxiq03@gmail.com'],
+        os.getenv("EMAIL_HOST_USER"),
+        ['test@gmail.com'],
     )
 
 
 class Check(APIView):
+    # Note: when mentor click on a link, the browser sends a get request
     def get(self, request):
         username = request.GET.get('username', '')
         first_name = request.GET.get('first_name', '')
@@ -53,22 +60,23 @@ class Check(APIView):
         number_phone = request.GET.get('number_phone', '')
         password = request.GET.get('password', '')
 
-        user = User(username=username, first_name=first_name, last_name=last_name, email=email)
-        user.set_password(password)
-        user.save()
-        profile = Profile(user=user, github=github, number_phone=number_phone, password=password)
-        profile.save()
-        return Response({"success": "Account created "})
+        try:
+            user = User(username=username, first_name=first_name, last_name=last_name, email=email)
+            user.set_password(password)
+            user.save()
+            profile = Profile(user=user, github=github, number_phone=number_phone, password=password)
+            profile.save()
+            return Response({"success": "Аккаунт создан "})
+        except IntegrityError:
+            return Response({"message": "Аккаунт уже создан "})
 
 
 class Login(APIView):
     def get(self, request):
         user = authenticate(username=request.GET.get('username', ''), password=request.GET.get('password', ''))
-        token = Token.objects.get(user=user)
-        print(token)
         if user is not None:
             login(request, user)
-            return Response(status=200, data={"token": str(token)})
+            return Response(status=200, data={"token": str(Token.objects.get(user=user))})
         else:
             return Response(status=403, data={"message": "Неправильный ввод данных"})
 
