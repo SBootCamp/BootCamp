38bcddcc91957ffe1653eb73110cef5ac732dcf6
Dillag <leha88088@gmail.com>
2022-03-17 14:24:18 +0300

    ready to merge, add tree for auth user, add trees for other users, add crud events new: delete CrUd from serializer and add them to views, clear migrations, add "include" to urls, clear import, delete all prints, changed "filter" to "get", add "try...except", refactor add choice.py, add values_list, add new except, use choice.py:)

--- BootCamp/roadmap/admin.py
+++ BootCamp/roadmap/admin.py
@@ -3,7 +3,6 @@ from mptt.admin import MPTTModelAdmin
 from .models import *
 
 class CustomMPTTModelAdmin(MPTTModelAdmin):
-    # specify pixel amount for this ModelAdmin only:
     mptt_level_indent = 20
 
 admin.site.register(Node, CustomMPTTModelAdmin)
